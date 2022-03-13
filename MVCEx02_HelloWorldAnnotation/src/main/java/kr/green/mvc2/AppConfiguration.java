ad14031c8bf076e5d0607ecce845814b56c69b38
Moonhee Hong <hmh94253862@gmail.com>
2022-03-13 18:16:38 +0900

    MVCEx02_HelloWorldAnnotation

---
+++ MVCEx02_HelloWorldAnnotation/src/main/java/kr/green/mvc2/AppConfiguration.java
@@ -0,0 +1,26 @@
+package kr.green.mvc2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "kr.green.mvc2")
+public class AppConfiguration {
+	
+	@Bean(name="HelloWorld")
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+
+}
